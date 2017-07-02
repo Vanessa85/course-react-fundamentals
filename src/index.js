4194b21f79c53260631e69ee554fce7edb170c9f
Vanessa Milagros <vanessapr.85@gmail.com>
2017-07-01 22:52:31 -0500

    added prop-types

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>Hello world</div>
+      <div>{ this.props.message }</div>
     );
   }
 }
 
+App.propTypes = {
+  message: PropTypes.string.isRequired
+};
 
 ReactDOM.render(
-  <App />,
+  <App message="Hello world" />,
   document.querySelector('#app')
 );
