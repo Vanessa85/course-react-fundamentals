708156db07fb08410d0600189709de9a99b00e95
Vanessa Milagros <vanessapr.85@gmail.com>
2017-07-02 09:31:18 -0500

    added axios and api.js

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import api from '../utils/api';
 
 function SelectLanguage(props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -28,24 +29,36 @@ class Popular extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
   updateLanguage(lang) {
     this.setState(function() {
-      return { selectedLanguage: lang  };
+      return { selectedLanguage: lang, repos: null  };
+    });
+
+    api.fetchPopularRepos(lang).then(repos => {
+      this.setState({ repos: repos });
     });
   }
 
   render() {
 
     return (
-      <SelectLanguage
-        selectedLanguage={this.state.selectedLanguage}
-        onSelect={this.updateLanguage} />
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage} />
+        { JSON.stringify(this.state.repos, null, 2) }
+      </div>
     );
   }
 }
