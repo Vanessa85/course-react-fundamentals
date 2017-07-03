fbdf4a44e074facc8898fc4172056611fd321b89
Vanessa Milagros <vanessapr.85@gmail.com>
2017-07-03 07:29:32 -0500

    added methods for battle in api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,63 @@
 import axios from 'axios';
 
-export default {
-  fetchPopularRepos: function(language) {
-    var encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=starts:>1+language:${language}&sort=starts&order=desc&type=Repositories`);
+const ID = '';
+const SECRET = '';
+var params = `?client_id=${ID}&client_secret=${SECRET}`;
 
+function getProfile(username) {
+  return axios.get(`https://api.github.com/users/${username}${params}`)
+    .then(response => response.data);
+}
+
+function getRepos(username) {
+  return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+}
+
+function getStartCount(repos) {
+  return repos.data.reduce((count, repo) => {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore(profile, repos) {
+  var followers = profile.followers;
+  var totalStarts = getStartCount(repos);
+  return (followers * 3) + totalStarts;
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(data => {
+    var profile = data[0];
+    var repos = data[1];
+    return {
+      profile, profile,
+      score: calculateScore(profile, repos)
+    };
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort((a, b) => {
+    return b.score - a.score;
+  });
+}
+
+export default {
+  battle: function(players) {
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError);
+  },
+  fetchPopularRepos: function(language) {
+    var encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
     return axios.get(encodedURI).then(response => response.data.items);
   }
 }
