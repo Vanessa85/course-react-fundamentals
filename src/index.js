007b810f21c11af9536d97752f9722219b486451
Vanessa Milagros <vanessapr.85@gmail.com>
2017-07-03 20:29:43 -0500

    added redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
 import './index.css';
 
+var store = createStore((state = 0, action) => {
+  return state;
+});
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#app')
 );
